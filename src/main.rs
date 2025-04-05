e7dc32ec6eab582f8ce9ef72edc89a715f7db2ff
Gabie <jacadel1998@hotmail.com>
2025-04-05 22:22:02 +0200

    Small warning change when no deps file is found

--- src/main.rs
+++ src/main.rs
@@ -568,7 +568,7 @@ fn main() {
                 let config_path = module_path.join(Path::new("ryna_deps.yml"));
     
                 if !config_path.exists() {
-                    ryna_warning!("Could not find ryna_deps.yml at the root of the library (perhaps you installed multiple libraries at once?)");
+                    ryna_warning!("Could not find ryna_deps.yml at the root of the library (perhaps you installed a wrong repository?)");
                     continue;
                 
                 } else {
